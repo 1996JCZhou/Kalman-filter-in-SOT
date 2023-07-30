2befc9c6114a6b19d2ebbdfedcbbd06a79ebd7a9
jiachen.zhou.VZH <jiachen.zhou@student.kit.edu>
2023-07-30 18:27:12 +0200

    Delete scripts of no use.

--- cal_FPS.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-video = cv2.VideoCapture("D:\\Object Detection\\SOT\\Kalman filter in SOT\\data\\testvideo1.mp4")
-
-FPS = video.get(cv2.CAP_PROP_FPS) # Read video FPS.
-print(FPS)
-
-video.release()
--- count_frames.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-video = cv2.VideoCapture("D:\\Object Detection\\SOT\\Kalman filter in SOT\\data\\testvideo1.mp4")
-
-frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) # Read video frames.
-print(frame_count)
-
-video.release()
